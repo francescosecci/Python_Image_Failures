08e012cd305026d59ab01fa383f2a5dec277abc2
Francesco Secci <i_secci@hotmail.it>
2020-06-19 11:17:17 +0200

    Update noise.py

--- noise.py
+++ noise.py
@@ -5,7 +5,6 @@ from matplotlib import pyplot as plt
 img = cv2.imread('sim.jpg')
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-
 #Speckle Noise
 gauss = np.random.normal(0,1,img.size)
 gauss = gauss.reshape(img.shape[0],img.shape[1],img.shape[2]).astype('uint8')
