0f986ff36d70db7e8666f3da25397a081aeb4c6a
AndreaCeccarelli <andrea.ceccarelli@unifi.it>
2020-09-02 15:17:01 +0200

    Update deadpixel.py

--- deadpixel.py
+++ deadpixel.py
@@ -3,31 +3,17 @@ import cv2
 from matplotlib import pyplot as plt
 from PIL import Image
 
+img= cv2.imread('sim1.jpg')
+img1 = cv2.imread('sim1.jpg')
+img1 = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+h, w, _ = img1.shape
+#set  pixel at coordinates 90, 150 to black color
+w=150
+h=90
+img1[h,w]=(0,0,0)
+img1 = Image.fromarray(img1)
 
-img = cv2.imread('sim1.jpg')
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-'''
-If the 'ndarray' of the image read by OpenCV imread() is converted to a
-'PIL.Image' object and saved, the image with the wrong color is saved.
-
-If you want to convert 'ndarray' and 'PIL.Image' objects to use both Pillow
-and OpenCV functions, you need to convert BGR and RGB.
-'''
-
-img1 = Image.open('sim1.jpg')
-
-pixels = img1.load()
-
-width, height = img1.size
-
-for i in range(0, width):
-    for j in range(0, height):
-        if i==150 and j==90:
-            pixels[i,j] = (255,0,0) # rosso per notarlo meglio nell'immagine
-
-img1 = np.array(img1) # esempio trasformazione da PIL.JpegImagePlugin.JpegImageFile
-                      # a 'numpy.ndarray'
-
+    
 plt.figure(num='Fallimento DEADPIXEL')
 plt.subplot(121),plt.imshow(img),plt.title('Originale')
 plt.xticks([]), plt.yticks([])
