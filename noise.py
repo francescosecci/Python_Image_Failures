8f1b2be610a177ce51318e4879ee0d26dbf84413
AndreaCeccarelli <andrea.ceccarelli@unifi.it>
2020-11-03 18:11:25 +0100

    Update noise.py

--- noise.py
+++ noise.py
@@ -6,6 +6,7 @@ img = cv2.imread('sim.jpg')
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 #Speckle Noise
+#second value (the "1") is the Standard deviation (spread or "width") of the distribution 
 gauss = np.random.normal(0,1,img.size)
 gauss = gauss.reshape(img.shape[0],img.shape[1],img.shape[2]).astype('uint8')
 noise = img + img * gauss
