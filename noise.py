ae1faf7244943cb1796c0b0644d2fdf65898e705
AndreaCeccarelli <andrea.ceccarelli@unifi.it>
2020-11-03 18:11:51 +0100

    Update noise.py

--- noise.py
+++ noise.py
@@ -11,8 +11,8 @@ gauss = np.random.normal(0,1,img.size)
 gauss = gauss.reshape(img.shape[0],img.shape[1],img.shape[2]).astype('uint8')
 noise = img + img * gauss
 
-plt.figure(num='Fallimento NOISE')
-plt.subplot(121),plt.imshow(img),plt.title('Originale')
+plt.figure(num='Failure NOISE')
+plt.subplot(121),plt.imshow(img),plt.title('Original')
 plt.xticks([]), plt.yticks([])
 plt.subplot(122),plt.imshow(noise),plt.title('Noise')
 plt.xticks([]), plt.yticks([])
